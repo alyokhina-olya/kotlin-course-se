0c1279329bd3fddd865c5da716da7a8dcd558ce7
alyokhina-o <alyokhina-o@yandex-team.ru>
2018-09-06 22:21:57 +0300

    [problem-113A] create file for test

---
+++ src/test/kotlin/ru/hse/spb/mainTest.kt
@@ -0,0 +1,21 @@
+package ru.hse.spb
+
+import org.junit.Assert
+import org.junit.Test
+
+class MainTest {
+
+    @Test
+    fun testRun1() {
+        Assert.assertEquals("YES", run(listOf("petr")))
+    }
+
+    @Test
+    fun testRun2() {
+        Assert.assertEquals("NO", run(listOf("etis atis animatis etis atis amatis")))
+    }
+    @Test
+    fun testRun3() {
+        Assert.assertEquals("YES", run(listOf("nataliala kataliala vetra feinites")))
+    }
+}
